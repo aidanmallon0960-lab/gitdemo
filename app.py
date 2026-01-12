5062c58cc7a1d21b974d6f53009a532512b76abc
Michael Roche <161171115+Mystii412@users.noreply.github.com>
2026-01-12 08:38:49 -0500

    Add print statements about cookies and pets

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+print('I like cookies')
+
 print('Hello I\'m here for git practice - Remote')
 
+print('Sometimes I think about how dogs are better than cats')
+
 print('Hello I\'m here for git practice - Local')
+
+print('Someimtes I realize that students don\'t do anything i want them to do')
 
 print("Added during merge")
